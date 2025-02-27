0417c7d0e50f5b3c4829f84f89e328eef2c13ea4
Starbucksbot <13952996+Starbucksbot@users.noreply.github.com>
2025-02-26 23:39:21 -0500

    fix server

--- server.js
+++ server.js
@@ -11,7 +11,14 @@ const port = config.PORT;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+
+// Serve static files (e.g., style.css, client.js) from the root
+app.use(express.static(path.join(__dirname)));
+
+// Serve index.html as the default route
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 let searchHistory = [];
 try {
